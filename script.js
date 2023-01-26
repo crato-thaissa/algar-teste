05b25e91240ec73458ddb39c2df4cf4a76debea6
Thaissa <you@example.com>
2023-01-26 13:26:40 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,74 @@
+/*const form = document.getElementById('form');
+const Tlocal = document.getElementById('Tlocal');
+const Tambiente = document.getElementById('Tambiente');
+const Tdivisoria = document.getElementById('Tdivisoria');
+const Tteto = document.getElementById('Tteto"');
+const Qtdandar = document.getElementById('Qtdandar');
+const Qtteto = document.getElementById('Qtteto');
+const critico = document.getElementById('critico');
+const Ssim = document.getElementById('Ssim');
+const Aswitch = document.getElementById('Aswitch');
+const POE = document.getElementById('POE');
+const estutura = document.getElementById('estutura');
+const form = document.getElementsByClassName('Tlocal');
+
+
+    form.addEventListener("submit", (e)  => {
+ e.preventDefault();
+
+    checkInputs();
+    });
+
+    function checkInputs(){
+        const TlocalValue = Tlocal.Value;
+       // const TambientelValue = Tambiente.Value;
+        //const TdivisoriaValue = Tdivisoria.Value;
+        //const TtetoValue = Tteto.Value;
+        //const QtdandarValue = Qtdandar.Value;
+        //const QttetoValue = Qtteto.Value;
+       // const criticoValue = critico.Value;
+       // const SsimValue = Ssim.Value;
+       // const AswitchValue = Aswitch.Value;
+       // const POEValue = POE.Value;
+        //const estuturaValue = estutura.Value;
+  /*  }
+
+    if (TlocalValue === "" ){
+        SetErrorFor(Tlocal, "Obrigatorio escolher um local.")
+    
+    }
+
+ function SetErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+    
+    small.innerText = message;
+    formControl.className = "form-control error";
+}
+function setsSucessFor(input){
+    const formControl = input.parentElement;
+
+    formControl.className = "form-control sucess";
+}
+
+*/
+
+
+const form = document.getElementById('form');
+const Qtdandar = document.querySelectorAll('required');
+const form-control = document.getElementById('form-control');
+
+
+function selectvalidate () {
+    if(campos[0].value.length === null)
+
+}
+else{
+    console.log('selecionado');
+}
+
+
+
+
+
+
